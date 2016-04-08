5a2326eedf8873bb8ded1811eff89f4cf76415c6
Jonathan Guillotte-Blouin <jonathan.guillotte.blouin@gmail.com>
2016-04-08 01:47:01 -0500

    added and tested simple query to postgres db

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express'),
       http    = require('http'),
       path = require('path'),
-      handlebars = require('express-handlebars');
+      handlebars = require('express-handlebars'),
+      pg = require('pg');
 
 var app = express(),
     hbs = handlebars.create({
       defaultLayout: 'main'
-    });
+    }),
+    conString = 'postgres://postgres:root@localhost:5432/projet-csi2532';
 
 app.set('port', 5000);
 app.set('views', path.join(__dirname, 'views'));
@@ -19,3 +21,18 @@ http.createServer(app).listen(app.get('port'), function(){
 });
 
 require('./router')(app);
+
+pg.connect(conString, (err, client, done) => {
+  if (err)
+    return console.error('error fetching client from pool', err);
+
+  client.query('INSERT INTO Secretaire VALUES($1, $2, $3, $4)', ["819-543-0112", "gui", "jon", "10 impasse nebuleuse"], (err) => {
+    if (err)
+      console.log('erreur insertion', err);
+    else {
+      console.log('success insertion');
+    }
+
+    done();
+  });
+});

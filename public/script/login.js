f5849339341eeaae5044a785a7f2873669ed65aa
Jonathan Guillotte-Blouin <jonathan.guillotte.blouin@gmail.com>
2016-04-10 02:14:15 -0400

    enhance UX - personalized forms - added datepicker

--- public/script/login.js
+++ public/script/login.js
@@ -1,8 +1,19 @@
 $(function() {
-  $('input:radio').change(function(){
+  $('#typeChoix :radio').change(function() {
     var choix = '#' + $(this).attr('id') + 'Form';
 
     $('.persoForm').hide();
     $(choix).slideDown();
+  });
+
+  $(':radio').change(function() {
+    $(this).parent().toggleClass('btn-default btn-primary');
+    $(this).parent().siblings().removeClass('btn-primary').addClass('btn-default');
+  });
+
+  $('.dateInput').datepicker({
+    format: "dd/mm/yyyy",
+    endDate: "today",
+    language: "fr"
   });
 });
